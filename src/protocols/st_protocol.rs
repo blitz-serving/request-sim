7c30f11834b63e1861cd3b19b96104d4f4ffd06c
Wang Haotian <WangHaotian013@outlook.com>
2024-09-05 13:04:33 +0000

    minor: adjust st_protocol random output

--- src/protocols/st_protocol.rs
+++ src/protocols/st_protocol.rs
@@ -14,7 +14,7 @@ pub struct StProtocol {
 impl StProtocol {
     /// Current the randomly generated token ids are in the range of 0..10000.
     pub fn new(tokenizer: Tokenizer) -> Self {
-        let target_token = tokenizer.encode("Hello World", true).unwrap().get_ids()[1];
+        let target_token = tokenizer.encode("Hello World", false).unwrap().get_ids()[1];
         Self {
             tokenizer,
             target_token,
@@ -106,36 +106,39 @@ impl Protocol for StProtocol {
 
 #[cfg(test)]
 mod tests {
-    use rand::{thread_rng, Rng};
     use serde_json::json;
 
     use super::*;
 
+    const TOKENIZER_PATH: &str = "/nvme/huggingface/hub/opt-1.3b/tokenizer.json";
+    // const TOKENIZER_PATH: &str = "/nvme/huggingface/hub/Llama-2-7b-hf/tokenizer.json";
+
     #[test]
     fn test_tokenizer() {
-        if std::path::Path::new("/nvme/huggingface/hub/Llama-2-7b-hf/tokenizer.json").exists() {
-            let tokenizer =
-                Tokenizer::from_file("/nvme/huggingface/hub/Llama-2-7b-hf/tokenizer.json").unwrap();
+        if std::path::Path::new(TOKENIZER_PATH).exists() {
+            let tokenizer = Tokenizer::from_file(TOKENIZER_PATH).unwrap();
             let vocab_size = tokenizer.get_vocab_size(false);
             println!("{:?}", vocab_size);
+
             let encodings = tokenizer
                 .encode("Hello World", false)
                 .unwrap()
                 .get_ids()
                 .to_vec();
             println!("{:?}", encodings);
+
             let sentence = tokenizer.decode(&encodings, false).unwrap();
             println!("{:?}", sentence);
+
             let words = encodings
                 .iter()
                 .map(|a| tokenizer.id_to_token(*a).unwrap())
                 .collect::<Vec<_>>();
             println!("{:?}", words);
-            let input_token_ids = (0..100)
-                .map(|_| thread_rng().gen_range(0..(tokenizer.get_vocab_size(false) as u32)))
-                .collect::<Vec<_>>();
-            let json_body = serde_json::json!({"input":tokenizer.decode(&input_token_ids, false).unwrap(),"parameter":{"max_new_tokens":100}});
-            println!("{}", json_body.to_string());
+
+            let input = vec![encodings[1]; 100 as usize];
+            let input = tokenizer.decode(&input, false).unwrap();
+            println!("{:?}", input);
         } else {
             print!("Tokenizer file not found");
         }
@@ -143,8 +146,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_st_protocol() {
-        let tokenizer =
-            Tokenizer::from_file("/nvme/huggingface/hub/Llama-2-7b-hf/tokenizer.json").unwrap();
+        let tokenizer = Tokenizer::from_file(TOKENIZER_PATH).unwrap();
         let st_protocol = StProtocol::new(tokenizer);
         let json_body = st_protocol.request_json_body(100, 100);
         println!("{}", json_body);
